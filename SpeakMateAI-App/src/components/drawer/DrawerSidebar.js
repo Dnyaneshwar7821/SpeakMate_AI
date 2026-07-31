143be996f653e51c7614c9cb3ff34a63c4d4fde0
Dnyaneshwar Algule <dnyaneshwaralgule2003@gmail.com>
2026-07-31 11:35:24 +0530

    fix: add missing useMemo import in DrawerSidebar

--- SpeakMateAI-App/src/components/drawer/DrawerSidebar.js
+++ SpeakMateAI-App/src/components/drawer/DrawerSidebar.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
 
 import {
   Alert,
