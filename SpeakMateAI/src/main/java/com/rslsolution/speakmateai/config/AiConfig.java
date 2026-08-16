1d520b5ad53dd9813b9f7a800abc1c2f75301085
Dnyaneshwar Algule <dnyaneshwaralgule2003@gmail.com>
2026-08-16 23:20:40 +0530

    feat: Upgrade web application UI with Tailwind v4, modern glassmorphism, animated visualizers, and polished design system

--- SpeakMateAI/src/main/java/com/rslsolution/speakmateai/config/AiConfig.java
+++ SpeakMateAI/src/main/java/com/rslsolution/speakmateai/config/AiConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class GeminiConfig {
+public class AiConfig {
 
 	@Bean
 	public RestTemplate restTemplate() {
