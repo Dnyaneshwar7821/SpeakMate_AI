b80fd62ff0c4d3e1995468b7215d21dbb3dfb29c
Dnyaneshwar Algule <dnyaneshwaralgule2003@gmail.com>
2026-08-28 14:01:57 +0530

    feat: align forgot password flow with mobile, add constructors to auth DTOs, add live search dropdown in navbar

--- SpeakMateAI/src/main/java/com/rslsolution/speakmateai/dto/request/ForgotPasswordRequest.java
+++ SpeakMateAI/src/main/java/com/rslsolution/speakmateai/dto/request/ForgotPasswordRequest.java
@@ -2,9 +2,15 @@ package com.rslsolution.speakmateai.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ForgotPasswordRequest {
 
 	@NotBlank(message = "Email is required")
