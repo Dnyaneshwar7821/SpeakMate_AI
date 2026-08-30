d418010381e91a3f42fd5f2e2bd1700a873f7473
Dnyaneshwar Algule <dnyaneshwaralgule2003@gmail.com>
2026-08-30 23:35:06 +0530

    Fix useEffect import in mobile NotificationsScreen.js

--- SpeakMateAI-App/src/screens/main/NotificationsScreen.js
+++ SpeakMateAI-App/src/screens/main/NotificationsScreen.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import {
   Alert,
